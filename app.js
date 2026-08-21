22d63c3d98bc53ac2ac115dbe5c9ea3523831aec
PrasannaPradhan1025 <prasanna.pradhan179@gmail.com>
2026-08-21 21:04:21 +0545

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+import { PORT } from "./config/env.js";
+
+
+app.get('/',(req , res)=>{
+    res.send('Welcome to the Subscription Tracer API');
+})
+
+app.listen(PORT , ()=>{
+  console.log(`Server is running on http://localhost:${PORT}`);
+    
+})
+
+export default app;
